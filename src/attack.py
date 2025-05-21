8493710a8e33b8d5db915827844f6ce938b12206
Gilad Freidkin <freidkin.gilad@gmail.com>
2025-05-21 14:27:19 +0300

    rewritten everything for support for kv-caching, and cleaner implementation, and working with input_ids directly.

--- src/attack.py
+++ src/attack.py
@@ -1,4 +1,4 @@
-from src.embed_injector import EmbedInjector
+from src.adver_model import AdverModel
 import torch
 from abc import ABC, abstractmethod
 
@@ -6,10 +6,10 @@ from abc import ABC, abstractmethod
 class Attack(ABC):
     def __init__(
         self,
-        embed_injector: EmbedInjector,
+        adv_model: AdverModel,
         silent: bool = False,
     ):
-        self.embed_injector = embed_injector
+        self.adv_model = adv_model
         self.silent = silent
 
     @property
@@ -17,14 +17,28 @@ class Attack(ABC):
         """
         Returns the number of adversarial tokens to be injected.
         """
-        return self.embed_injector.num_tokens
+        return self.adv_model.num_tokens
 
     @property
     def device(self) -> torch.device:
         """
         Returns the device on which the model is located.
         """
-        return self.embed_injector.device
+        return self.adv_model.device
+
+    @property
+    def embed_dim(self) -> int:
+        """
+        Returns the embedding dimension of the model.
+        """
+        return self.adv_model.embed_dim
+
+    @property
+    def embed_dtype(self) -> torch.dtype:
+        """
+        Returns the embedding dtype of the model.
+        """
+        return self.adv_model.embed_dtype
 
     def align_preds(
         self,
@@ -51,12 +65,20 @@ class Attack(ABC):
         pred_logits = logits[target_mask].view(-1, logits.size(-1))
         return pred_logits, target_ids
 
+    def init_embedding(self, num_inputs: int, **kwargs) -> torch.Tensor:
+        return torch.randn(
+            size=(num_inputs, self.num_tokens, self.embed_dim),
+            device=self.device,
+            dtype=self.embed_dtype,
+            requires_grad=True,
+        )
+
     @abstractmethod
     def fit(
         self,
         input_texts: list[str],
         target_texts: list[str],
-        init_embedding: torch.Tensor | None = None,
+        adv_embeds: torch.Tensor | None = None,
     ) -> torch.Tensor:
         """
         Fit the attack model to the input and target texts.
@@ -64,7 +86,7 @@ class Attack(ABC):
         Args:
             input_texts (list[str]): List of input texts.
             target_texts (list[str]): List of target texts.
-            init_embedding (torch.Tensor | None): Initial adversarial embedding.
+            adv_embeds (torch.Tensor | None): Initial adversarial embedding.
 
         Returns:
             torch.Tensor: Adversarial embedding of shape (batch_size, num_tokens, embedding_dim).
--- src/attacks/optim_attack.py
+++ src/attacks/optim_attack.py
@@ -1,89 +1,80 @@
 from typing import Callable, Iterable
-from src.embed_injector import EmbedInjector
+from src.adver_model import AdverModel
 from src.attack import Attack
 from tqdm.auto import tqdm
 import torch
+import copy
 
 
 class OptimAttack(Attack):
     def __init__(
         self,
-        embed_injector: EmbedInjector,
+        adv_model: AdverModel,
         optim_factory: Callable[[Iterable[torch.Tensor]], torch.optim.Optimizer],
         steps: int = 100,
         mixed_precision: bool = True,
+        caching: bool = True,
         silent: bool = False,
     ):
-        super().__init__(embed_injector, silent)
+        super().__init__(adv_model, silent)
 
         self.steps = steps
         self.optim_factory = optim_factory
         self.mixed_precision = mixed_precision
-
-    def init_embedding(self, orig_embeds: torch.Tensor) -> torch.Tensor:
-        return torch.randn(
-            size=(orig_embeds.size(0), self.num_tokens, orig_embeds.size(-1)),
-            device=self.device,
-            dtype=orig_embeds.dtype,
-            requires_grad=True,
-        )
+        self.caching = caching # TODO: implement switch to turn on or off kv-caching
 
     def fit(
         self,
         input_texts: list[str],
         target_texts: list[str],
-        init_embedding: torch.Tensor | None = None,
+        adv_embeds: torch.Tensor | None = None,
     ) -> torch.Tensor:
 
-        # TODO: KV-Cache doesnt work with the inputs_embeds for some reason
-        # figure out how to make it work, or is it a lost cause?
-        # if kv-cache doesnt work, then we should try to find a more packed way to store the
-        # input embeddings
+        token_dict = self.adv_model.tokenize(input_texts, target_texts)
 
-        embed_dict = self.embed_injector.embed_text(input_texts, target_texts)
-        orig_embeds = embed_dict["inputs_embeds"]
-
-        with torch.no_grad():
-            kv_idx = embed_dict["const_idx"].min()
-            kv_result = self.embed_injector.forward(
-                orig_embeds[:, :kv_idx],
-                embed_dict["attention_mask"][:, :kv_idx],
+        with (
+            torch.no_grad(),
+            torch.autocast(device_type=self.device.type, enabled=self.mixed_precision),
+        ):
+            # construct kv-cache for the constant, first part of the batch
+            kv_idx = token_dict["const_idx"].min().item()
+            kv_result = self.adv_model.forward(
+                token_dict["input_ids"][:, :kv_idx],
+                token_dict["attention_mask"][:, :kv_idx],
                 use_cache=True,
             )
-            kv_cache = kv_result.past_key_values
+            kv_cache = copy.deepcopy(kv_result.past_key_values)
 
-        if init_embedding is not None:
-            adv_embed = init_embedding.clone().detach()
-            adv_embed.requires_grad_(True)
+        if adv_embeds is not None:
+            adv_embeds = adv_embeds.clone().detach()
+            adv_embeds.requires_grad_(True)
         else:
-            adv_embed = self.init_embedding(orig_embeds)
+            adv_embeds = self.init_embedding(num_inputs=len(input_texts))
 
         scaler = torch.GradScaler(enabled=self.mixed_precision)
-        optim = self.optim_factory([adv_embed])
+        optim = self.optim_factory([adv_embeds])
 
         with tqdm(range(self.steps), disable=self.silent, leave=False) as pbar:
-
             for step in pbar:
 
                 optim.zero_grad()
 
                 with torch.autocast(device_type=self.device.type, enabled=self.mixed_precision):
 
-                    inj_embeds = self.embed_injector.inject_embedding(
-                        inp_embeds=orig_embeds,
-                        adv_embeds=adv_embed,
-                        adv_mask=embed_dict["adv_mask"],
+                    result = self.adv_model.forward(
+                        input_ids=token_dict["input_ids"][:, kv_idx:],
+                        attention_mask=token_dict["attention_mask"],
+                        past_key_values=copy.deepcopy(kv_cache),  # NOTE: important to copy as forwrd modifies the cache in-place
+                        adv_embeds=adv_embeds,
+                        adv_mask=token_dict["adv_mask"][:, kv_idx:],
                     )
 
-                    result = self.embed_injector.forward(
-                        # input_ids=embed_dict["input_ids"][:, :kv_idx],
-                        inputs_embeds=inj_embeds[:, :kv_idx],
-                        labels=embed_dict["input_ids"],
-                        attention_mask=embed_dict["attention_mask"],
-                        past_key_values=kv_cache,
+                    pred_logits, target_ids = self.align_preds(
+                        result.logits,
+                        token_dict["input_ids"][:, kv_idx:],
+                        token_dict["target_mask"][:, kv_idx:],
                     )
 
-                    pred_logits, target_ids = self.align_preds(result.logits, embed_dict["input_ids"], embed_dict["target_mask"])
                     loss = torch.nn.functional.cross_entropy(pred_logits, target_ids)
 
                 scaler.scale(loss).backward()
@@ -92,4 +83,4 @@ class OptimAttack(Attack):
 
                 pbar.set_postfix({"loss": loss.item()})
 
-        return adv_embed.detach()
+        return adv_embeds.detach()
