63c479c095eff8bb7300544ba97494fa31be08e0
Gilad Freidkin <freidkin.gilad@gmail.com>
2025-07-17 12:48:46 +0300

    added inner-attack scheduler support.

--- src/univ_attacks/iml.py
+++ src/univ_attacks/iml.py
@@ -5,9 +5,8 @@ from src.eval.evaluator import Evaluator
 from src.config import GenConfig
 from src.univ_attacks.univ_attack import UnivAttack
 
-from typing import Any
+from typing import Any, Callable
 import torch
-from typing import Iterable, Callable
 
 
 # NOTE: currently loss is over all target tokens and not a single token per sample
@@ -40,15 +39,12 @@ def cosine_similarity_loss(
     return sample_losses.sum(dim=-1) / sample_mask.sum(dim=-1)
 
 
-# TODO: add scheduling to the inner-attack i.e accept a lambda function that takes the current epoch and
-# returns an instance of an inner attack.
-# TODO: implement discretization
 class IML(UnivAttack):
     def __init__(
         self,
         adv_model: AdvModel,
-        internal_attack: SampleAttack,
-        optim_factory: Callable[[Iterable[torch.Tensor]], torch.optim.Optimizer],
+        inner_attack: SampleAttack | Callable[[AdvModel, int], SampleAttack],
+        optimizer: torch.optim.Optimizer,
         activ_extractor: ActivationExtractor,
         evaluators: list[Evaluator],
         eval_freq: int | float = 1,
@@ -68,26 +64,41 @@ class IML(UnivAttack):
             log_dir=log_dir,
         )
 
-        self.internal_attack = internal_attack
+        if callable(inner_attack):
+            self.attack_builder_func = inner_attack
+            self.inner_attack = self.attack_builder_func(adv_model, 0)
+        else:
+            self.attack_builder_func = None
+            self.inner_attack = inner_attack
+
         self.activ_extractor = activ_extractor
-        self.optimizer = optim_factory([self.univ_embeds])
+        self.optimizer = optimizer
         self.skip_already_fooled = skip_already_fooled
         self.skip_failed_attacks = skip_failed_attacks
         self.dynamic_labels = dynamic_labels  # TODO: implement dynamic labels
 
         # TODO: (low priority) think of a better, less messy way to register hparams
-        self.logger.register_hparams({"iml/internal_attack": self.internal_attack.__class__.__name__})
+        self.logger.register_hparams({"iml/inner_attack": self.inner_attack.__class__.__name__})
         self.logger.register_hparams({"iml/optimizer": self.optimizer.__class__.__name__})
         self.logger.register_hparams({"iml/skip_already_fooled": self.skip_already_fooled})
         self.logger.register_hparams({"iml/skip_failed_attacks": self.skip_failed_attacks})
         self.logger.register_hparams({"iml/dynamic_labels": self.dynamic_labels})
 
         self.logger.register_hparams(activ_extractor.get_hparams())
-        self.logger.register_hparams({f"internal_attack/{k}": v for k, v in internal_attack.__dict__.items()})
-        self.logger.register_hparams({f"optim/name": self.optimizer.__class__.__name__})
+        self.logger.register_hparams({f"inner_attack/{k}": v for k, v in inner_attack.__dict__.items()})
+        self.logger.register_hparams({"optim/name": self.optimizer.__class__.__name__})
         self.logger.register_hparams({f"optim/{k}": v for k, v in self.optimizer.param_groups[0].items()})
 
+    def make_attack(self, epoch_num: int) -> SampleAttack:
+        if self.attack_builder_func is None:
+            return self.inner_attack
+        return self.attack_builder_func(self.adv_model, epoch_num)
+
     def optim_step(self, data: dict[str, list[Any]], epoch_num: int, batch_num: int) -> float | None:
+        # create new instance of inner attack for each epoch
+        if epoch_num > 0 and batch_num == 0:
+            self.inner_attack = self.make_attack(epoch_num)
+
         self.optimizer.zero_grad()
 
         # construct input conversations
@@ -112,7 +123,7 @@ class IML(UnivAttack):
             # run per-sample attack
             with torch.autocast(device_type=self.device.type, enabled=False):
                 init_embeds = self.univ_embeds.expand(len(input_convs), -1, -1)
-                attack_result = self.internal_attack.fit(input_convs, target_text, init_embeds=init_embeds)
+                attack_result = self.inner_attack.fit(input_convs, target_text, init_embeds=init_embeds)
                 sample_convs = attack_result.conversations
                 sample_embeds = attack_result.adv_embeds
 
